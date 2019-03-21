baf7b949b3c4aed276223dcacb74d04ab7d5bc3e
Jackie Carter <jacqueline.carter@hotmail.co.uk>
2019-03-21 16:52:46 +0000

    Updates during coding exercises

--- app/alarm.js
+++ app/alarm.js
@@ -11,24 +11,71 @@ function wakeup(currentTime) {
   // https://www.w3schools.com/jsref/jsref_obj_date.asp
   const currentHour = currentTime.getHours();
 
-  if (currentHour > 12) {
+  switch (currentHour) {
+    case 12:
+    case 13:
+    case 14:
+    return "Yessssss its lunch time!!!";
+    break;
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
     return "Ugggggh its too early - I'm not waking up yet";
-  } else {
+    break;
+  default:
     return "Get up!! You're wasting the day";
+    break;
   }
+  // alternative conditional statements for current hour processing
+ // if (currentHour  < 12) {
+  //  return "Ugggggh its too early - I'm not waking up yet";
+  //}  else if (currentHour >=12 && currentHour <=14) {
+   // return "Yessssss its lunch time!!!";
+  //} else {
+   // return "Get up!! You're wasting the day";
+  //}
 }
 
 function snooze() {
   let snoozeAttempts = 0;
 
-  for (let i = 0; i < 10; i++) {
+  while (snoozeAttempts < 20) {
     snoozeAttempts++;
   }
 
   return snoozeAttempts;
 }
+ // Alternative loop method for snooze
+ //for (let i = 0; i < 20; i++) {
+//  snoozeAttempts++;
+//}
+
+//return snoozeAttempts;
+//}
+
+// function to list names of alarm users
+function alarmUsers () {
+  // store alarm users in an array - names only
+  let namesofPeople = [];
+  peopleThatHaveUsedAlarm.forEach(function(person) {
+    namesofPeople.push(person.name);
+  });
+  // need to return values in function
+  return namesofPeople;
+  }
+
+//}
 
 module.exports = {
   wakeup,
-  snooze
+  snooze,
+  alarmUsers
 };
--- test/alarm.test.js
+++ test/alarm.test.js
@@ -5,7 +5,17 @@ test('Moans if too early', () => {
   timeNow.setHours(11);
   expect(alarm.wakeup(timeNow)).toBe("Ugggggh its too early - I'm not waking up yet");
 });
-
+test('Lunch time test', () => {
+  let timeNow = new Date();
+  timeNow.setHours(14);
+  expect(alarm.wakeup(timeNow)).toBe("Yessssss its lunch time!!!");
+});
 test('Snoozing for a set amount', () => {
-  expect(alarm.snooze()).toBe(10);
+  expect(alarm.snooze()).toBe(20);
+});
+//test('Alarm Users', () => {
+  // test names of users who have used alarm
+test('List Alarm Users', () => {
+ const expectedResult = ["James","Susan","Geoff","Lisa"];
+  expect(alarm.alarmUsers()).toEqual(expectedResult);
 });
